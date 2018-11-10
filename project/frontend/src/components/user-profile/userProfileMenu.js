79f047928c2621ee6003992018fc368d5145065d
Fábio Cordeiro <uc2016242575@student.uc.pt>
2018-11-10 20:12:01 +0000

    Auth 65%

--- project/frontend/src/components/user-profile/userProfileMenu.js
+++ project/frontend/src/components/user-profile/userProfileMenu.js
@@ -69,7 +69,7 @@ class UserProfileMenu extends Component {
                 <i className="fa fa-file-text" />
               </a>
             </div>
-            <a href="http://localhost:8000/api/user/authentication"><button>Visit Page Now</button></a>
+            <a href="http://localhost:8000/api/user/authentication"><button>Twitter Authentication</button></a>
           </div>
         </div>
         <UserProfileMenuSection data={this.props.data} toShow={this.state.showSection} />
