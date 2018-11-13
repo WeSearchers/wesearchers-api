35fc4d7a30c79198bd07bd0bd8494fb22e218554
Beatriz <geirinhas.beatriz@gmail.com>
2018-11-13 14:01:57 +0000

    Added upload button and fixed jumbotron design

--- project/frontend/src/components/feed/jumbotron.js
+++ project/frontend/src/components/feed/jumbotron.js
@@ -15,22 +15,20 @@ class Jumbotron extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
+      text: "",
       filename: null,
       media: null,
-      errors:{}
+      errors: {}
     };
     this.media = React.createRef();
     this.text = React.createRef();
   }
 
   handleChange = ev => {
-    if(ev.target.name === "media") {
-        this.setState({["media"]: ev.target.files[0]});
-        this.setState({["filename"]: ev.target.files[0].name});
-    }
-    else
-      this.setState({[ev.target.name]: ev.target.value});
+    if (ev.target.name === "media") {
+      this.setState({ ["media"]: ev.target.files[0] });
+      this.setState({ ["filename"]: ev.target.files[0].name });
+    } else this.setState({ [ev.target.name]: ev.target.value });
   };
 
   handleSubmit = ev => {
@@ -38,20 +36,19 @@ class Jumbotron extends Component {
     fd.append("text", this.state.text);
     fd.append("media", this.state.media, this.state.filename);
     Request.post("api/feed/publish", fd).then(response => {
-      if (response.status === 200){
+      if (response.status === 200) {
         this.text.value = "";
         this.media.value = "";
         this.setState({
-          text: '',
+          text: "",
           filename: null,
           media: null,
-          errors:{}
+          errors: {}
         });
-      }
-      else {
+      } else {
         response.json().then(errors => {
-          this.setState({errors: errors})
-        })
+          this.setState({ errors: errors });
+        });
       }
     });
   };
@@ -60,10 +57,10 @@ class Jumbotron extends Component {
     console.log(this.state.errors);
     return (
       <div className="write-pub bg-grey m-5 d-flex flex-column mr-auto ml-auto pb-3">
-        <div className="bla d-flex flex-column ">
+        <div className="bla d-flex flex-row ">
           <div className="d-flex flex-row align-content-baseline mb-4">
-          <div className="background-image-profile ml-3 mt-4" />
-              {/*{this.props.userData !== null && this.props.userData !== undefined ? (
+            <div className="background-image-profile ml-3 mt-4" />
+            {/*{this.props.userData !== null && this.props.userData !== undefined ? (
             <img
                   className="circle"
                   src={
@@ -72,10 +69,9 @@ class Jumbotron extends Component {
                   width={"100%"}
                   style={{}}
                 />
-                ) : null}*/
-              }
+                ) : null}*/}
           </div>
-          <div className="textField descrition mb-3 ">
+          <div className=" textField mb-3 mt-4">
             <textarea
               className="form-control z-depth-1 ml-5"
               id="exampleFormControlTextarea6"
@@ -83,32 +79,43 @@ class Jumbotron extends Component {
               name="text"
               ref={this.text}
               onChange={this.handleChange}
-              placeholder="Text..."
+              placeholder="Write here..."
             />
           </div>
           {this.state.errors.text !== undefined &&
-                this.state.errors.text !== null ? (
-                  <div className="wrongpass">
-                    <p>{this.state.errors.text}</p>
-                  </div>
-                ) : null}
+          this.state.errors.text !== null ? (
+            <div className="wrongpass">
+              <p>{this.state.errors.text}</p>
+            </div>
+          ) : null}
         </div>
-        <div className="buttons d-flex flex-row justify-content-end mr-4 mt-3">
-          <div className="btn-foto btn btn-light m-1">
-          <input id="f02" type="file" accept="image/*,video/*" name="media" ref={this.media} onChange={this.handleChange}/>
-            <label for="f02"><img className="pr-1" src={photo} width="18" height="18" />
-            Photo/Video
-            {this.props.buttonLabel}</label>
-            {this.state.errors.media !== undefined &&
-                this.state.errors.media !== null ? (
-                  <div className="wrongpass">
-                    <p>{this.state.errors.media}</p>
-                  </div>
-                ) : null}
+        <div className="buttons d-flex flex-row justify-content-between mr-4 mt-3">
+          <div className="input-jumbotron upload-btn ml-5">
+            <input
+              onChange={this.handlechange}
+              id="f02"
+              type="file"
+              name="image"
+              accept="image/*"
+              placeholder="Upload"
+            />
+            <label for="f02">Upload</label>
+            {this.state.errors.image !== undefined &&
+            this.state.errors.image !== null ? (
+              <div className="wrongpass">
+                <p>{this.state.errors.image}</p>
+              </div>
+            ) : null}
           </div>
+          {this.state.errors.media !== undefined &&
+          this.state.errors.media !== null ? (
+            <div className="wrongpass">
+              <p>{this.state.errors.media}</p>
+            </div>
+          ) : null}
           <button
             type="button"
-            className="btn-publish btn text-white m-1 ml-4 mt-2"
+            className="btn-publish btn text-white m-1 mr-2 mt-2 "
             onClick={this.handleSubmit}
           >
             Publish
