bbd5b8702b01289b45a7044828db11dbffade4e1
Beatriz <geirinhas.beatriz@gmail.com>
2018-12-09 21:23:18 +0000

    responsive tweaks in feed content

--- project/frontend/src/components/feed/redditpub.js
+++ project/frontend/src/components/feed/redditpub.js
@@ -21,7 +21,7 @@ class RedditPub extends React.Component {
       modal1: false,
       modal2: false,
       isLoading: true,
-      contacts: [],
+      contacts: []
     };
     this.togglemodal1 = this.togglemodal1.bind(this);
     this.togglemodal2 = this.togglemodal2.bind(this);
@@ -60,104 +60,108 @@ class RedditPub extends React.Component {
   };
 
   render() {
-      console.log(this.props.data);
+    console.log(this.props.data);
     return (
-      <div className="pub1 m-5 bg-grey d-flex flex-column mr-auto ml-auto ">
-        <div className="icons d-flex flex-row justify-content-end mr-4">
-          <img className="mt-3" src={save} width="14" height="14" />
-          <img className="mt-3 ml-3" src={trash} width="14" height="16" />
-        </div>
-        <div className=" d-flex flex-row align-content-baseline mb-4">
-          <div className="background-image-profile ml-3 mt-3">
-            {this.props.data !== null && this.props.data !== undefined ? (
-              <img
-                src={
-                  this.props.data !== null && this.props.data !== undefined
-                    ? this.props.data.profile_pic_url
-                    : null
-                }
-                width={"100%"}
-                style={{}}
-              />
-            ) : null}
-          </div>
-          <div className=" mt-4 ml-4  d-flex flex-column justify-content-center">
-            <p className="font-weight-bold mb-0">
-              {this.props.data !== null && this.props.data !== undefined
-                ? this.props.data.name
-                : "Name Surname"}
-            </p>
-            {/*Ze monteiro colocar nome a partir do fetch*/}
-            <p className="font-weight-light mb-0">
-              {/*Date and hour*/}
-              {this.props.data !== undefined && this.props.data !== null
-                ? this.props.data.date
-                : "yyyy/mm/dd"}
-            </p>
-          </div>
-        </div>
-        <div className="d-flex flex-row justify-content-end mr-5">
-            <p className="reddit-pub mt-2 mr-3 ml-4">From Reddit</p>
-          </div>
-        <div className="content d-flex flex-row justify-content-center bg-white mr-2 ml-2 mr-auto ml-auto">
-          {this.props.data !== undefined && this.props.data !== null ? (
-              <div>
-              <p className="font-weight-bold m-2 ml-4 mr-4 mt-4 mb-4">
-                  {this.props.data.title}
-              </p>
-            <p className="font-weight-light m-2 ml-4 mr-4 mt-4 mb-4">
-              {this.props.data.text}
-            </p>
+      <div className="container-pub">
+        <div className="row">
+          <div className="col-md-7 col-sm-10 col-xs-2 pub1 m-5 bg-grey d-flex flex-column mr-auto ml-auto ">
+            <div className="icons d-flex flex-row justify-content-end mr-4">
+              <img className="mt-3" src={save} width="14" height="14" />
+              <img className="mt-3 ml-3" src={trash} width="14" height="16" />
+            </div>
+            <div className=" d-flex flex-row align-content-baseline mb-4">
+              <div className="background-image-profile ml-3 mt-3">
+                {this.props.data !== null && this.props.data !== undefined ? (
+                  <img
+                    src={
+                      this.props.data !== null && this.props.data !== undefined
+                        ? this.props.data.profile_pic_url
+                        : null
+                    }
+                    width={"100%"}
+                    style={{}}
+                  />
+                ) : null}
               </div>
-          ) : null}
-        </div>
-        <div className="pub-btn d-flex flex-row justify-content-end ">
-          <button
-            type="button"
-            className="btn-seemore text-white btn btn-secondary m-1 mr-4 mt-3"
-            onClick={this.seeMore}
-          >
-            See more
-          </button>
-        </div>
-        {this.props.data !== undefined &&
-        this.props.data !== null &&
-        this.props.data.media_url !== null ? (
-          <div className="anexo d-flex flex-row justify-content-center mr-2 ml-2 mt-5 mr-auto ml-auto text-black-50">
-            <img
-              src={this.props.data.media_url}
-              style={{ width: "100%", height: "100%" }}
-            />
-          </div>
-        ) : null}
-        <div className="barra d-flex flex-row ml-4 mt-3">
-          <div className="icons d-flex flex-row mb-4">
-            <i className="fa fa-arrow-up" />
-            <p>
-              {" "}
-              {this.props.data !== undefined && this.props.data !== null
-                ? this.props.data.score
-                : null}{" "}
-            </p>
-            <i className="fa fa-balance-scale" />
-            <p>
-              {" "}
-              {this.props.data !== undefined && this.props.data !== null
-                ? this.props.data.ratio
-                : null}{" "}
-            </p>
-          </div>
-          <div className=" d-flex flex-row justify-content-end hashtags mr-4">
-            {this.props.data !== undefined && this.props.data !== null
-              ? (
+              <div className=" mt-4 ml-4  d-flex flex-column justify-content-center">
+                <p className="font-weight-bold mb-0">
+                  {this.props.data !== null && this.props.data !== undefined
+                    ? this.props.data.name
+                    : "Name Surname"}
+                </p>
+                {/*Ze monteiro colocar nome a partir do fetch*/}
+                <p className="font-weight-light mb-0">
+                  {/*Date and hour*/}
+                  {this.props.data !== undefined && this.props.data !== null
+                    ? this.props.data.date
+                    : "yyyy/mm/dd"}
+                </p>
+              </div>
+            </div>
+            <div className="d-flex flex-row justify-content-end mr-5">
+              <p className="reddit-pub mt-2 mr-3 ml-4">
+                From <span> Reddit </span>
+              </p>
+            </div>
+            <div className="content d-flex flex-row justify-content-center bg-white mr-2 ml-2 mr-auto ml-auto">
+              {this.props.data !== undefined && this.props.data !== null ? (
+                <div>
+                  <p className="font-weight-bold m-2 ml-4 mr-4 mt-4 mb-4">
+                    {this.props.data.title}
+                  </p>
+                  <p className="font-weight-light m-2 ml-4 mr-4 mt-4 mb-4">
+                    {this.props.data.text}
+                  </p>
+                </div>
+              ) : null}
+            </div>
+            <div className="pub-btn d-flex flex-row justify-content-end ">
+              <button
+                type="button"
+                className="btn-seemore text-white btn btn-secondary m-1 mr-4 mt-3"
+                onClick={this.seeMore}
+              >
+                See more
+              </button>
+            </div>
+            {this.props.data !== undefined &&
+            this.props.data !== null &&
+            this.props.data.media_url !== null ? (
+              <div className="anexo d-flex flex-row justify-content-center mr-2 ml-2 mt-5 mr-auto ml-auto text-black-50">
+                <img
+                  src={this.props.data.media_url}
+                  style={{ width: "100%", height: "100%" }}
+                />
+              </div>
+            ) : null}
+            <div className="barra d-flex flex-row ml-4 mt-3">
+              <div className="icons d-flex flex-row mb-4">
+                <i className="fa fa-arrow-up mr-2" />
+                <p>
+                  {" "}
+                  {this.props.data !== undefined && this.props.data !== null
+                    ? this.props.data.score
+                    : null}{" "}
+                </p>
+                <i className="fa fa-balance-scale mr-2 ml-4" />
+                <p>
+                  {" "}
+                  {this.props.data !== undefined && this.props.data !== null
+                    ? this.props.data.ratio
+                    : null}{" "}
+                </p>
+              </div>
+              <div className=" d-flex flex-row justify-content-end hashtags mr-4">
+                {this.props.data !== undefined && this.props.data !== null ? (
                   <p className="bg-secondary text-white  text-center align-middle mr-1 first m-2">
-                      r/{this.props.data.subreddit}
-                    </p>
-                  )
-              : null}
-            <p className="bg-blue text-white  text-center align-middles mr-1 second  m-2">
-              ...
-            </p>
+                    r/{this.props.data.subreddit}
+                  </p>
+                ) : null}
+                <p className="bg-blue text-white  text-center align-middles mr-1 second  m-2">
+                  ...
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- project/frontend/src/components/feed/tweetpub.js
+++ project/frontend/src/components/feed/tweetpub.js
@@ -64,99 +64,107 @@ class TweetPub extends React.Component {
   render() {
     const { isLoading, contacts } = this.state;
     return (
-      <div className="pub1 m-5 bg-grey d-flex flex-column mr-auto ml-auto ">
-        <div className="icons d-flex flex-row justify-content-end mr-4">
-          <img className="mt-3" src={save} width="14" height="14" />
-          <img className="mt-3 ml-3" src={trash} width="14" height="16" />
-        </div>
-        <div className=" d-flex flex-row align-content-baseline mb-4">
-          <div className="background-image-profile ml-3 mt-3">
-            {this.props.data !== null && this.props.data !== undefined ? (
-              <img
-                src={
-                  this.props.data !== null && this.props.data !== undefined
-                    ? this.props.data.profile_pic_url
-                    : null
-                }
-                width={"100%"}
-                style={{}}
-              />
+      <div className="container-pub">
+        <div className="row">
+          <div className="col-md-12 col-sm-12 col-xs-12 pub1 m-5 bg-grey mr-auto ml-auto ">
+            <div className="icons d-flex flex-row justify-content-end mr-4">
+              <img className="mt-3" src={save} width="14" height="14" />
+              <img className="mt-3 ml-3" src={trash} width="14" height="16" />
+            </div>
+            <div className=" d-flex flex-row align-content-baseline mb-4">
+              <div className="background-image-profile ml-3 mt-3">
+                {this.props.data !== null && this.props.data !== undefined ? (
+                  <img
+                    src={
+                      this.props.data !== null && this.props.data !== undefined
+                        ? this.props.data.profile_pic_url
+                        : null
+                    }
+                    width={"100%"}
+                    style={{}}
+                  />
+                ) : null}
+              </div>
+              <div className=" mt-4 ml-4  d-flex flex-column justify-content-center">
+                <p className="font-weight-bold mb-0">
+                  {this.props.data !== null && this.props.data !== undefined
+                    ? this.props.data.name
+                    : "Name Surname"}
+                </p>
+                {/*Ze monteiro colocar nome a partir do fetch*/}
+                <p className="font-weight-light mb-0">
+                  {/*Date and hour*/}
+                  {this.props.data !== undefined && this.props.data !== null
+                    ? this.props.data.date
+                    : "yyyy/mm/dd"}
+                </p>
+              </div>
+            </div>
+            <div className="d-flex flex-row justify-content-end mr-5">
+              <p className="twitter-pub mt-2 mr-3 ml-4">
+                From <span>Twitter</span>
+              </p>
+            </div>
+            <div className="content d-flex flex-row justify-content-center bg-white mr-2 ml-2 mr-auto ml-auto">
+              {this.props.data !== undefined && this.props.data !== null ? (
+                <p className="font-weight-light m-2 ml-4 mr-4 mt-4 mb-4">
+                  {this.props.data.text}
+                </p>
+              ) : null}
+            </div>
+            <div className="pub-btn d-flex flex-row justify-content-end ">
+              <button
+                type="button"
+                className="btn-seemore text-white btn btn-secondary m-1 mr-4 mt-3"
+                onClick={this.seeMore}
+              >
+                See more
+              </button>
+            </div>
+            {this.props.data !== undefined &&
+            this.props.data !== null &&
+            this.props.data.media_url !== null ? (
+              <div className="anexo d-flex flex-row justify-content-center mr-2 ml-2 mt-5 mr-auto ml-auto text-black-50">
+                <img
+                  src={this.props.data.media_url}
+                  style={{ width: "100%", height: "100%" }}
+                />
+              </div>
             ) : null}
-          </div>
-          <div className=" mt-4 ml-4  d-flex flex-column justify-content-center">
-            <p className="font-weight-bold mb-0">
-              {this.props.data !== null && this.props.data !== undefined
-                ? this.props.data.name
-                : "Name Surname"}
-            </p>
-            {/*Ze monteiro colocar nome a partir do fetch*/}
-            <p className="font-weight-light mb-0">
-              {/*Date and hour*/}
-              {this.props.data !== undefined && this.props.data !== null
-                ? this.props.data.date
-                : "yyyy/mm/dd"}
-            </p>
-          </div>
-        </div>
-        <div className="d-flex flex-row justify-content-end mr-5">
-            <p className="reddit-pub mt-2 mr-3 ml-4">From Twitter</p>
-          </div>
-        <div className="content d-flex flex-row justify-content-center bg-white mr-2 ml-2 mr-auto ml-auto">
-          {this.props.data !== undefined && this.props.data !== null ? (
-            <p className="font-weight-light m-2 ml-4 mr-4 mt-4 mb-4">
-              {this.props.data.text}
-            </p>
-          ) : null}
-        </div>
-        <div className="pub-btn d-flex flex-row justify-content-end ">
-          <button
-            type="button"
-            className="btn-seemore text-white btn btn-secondary m-1 mr-4 mt-3"
-            onClick={this.seeMore}
-          >
-            See more
-          </button>
-        </div>
-        {this.props.data !== undefined &&
-        this.props.data !== null &&
-        this.props.data.media_url !== null ? (
-          <div className="anexo d-flex flex-row justify-content-center mr-2 ml-2 mt-5 mr-auto ml-auto text-black-50">
-            <img
-              src={this.props.data.media_url}
-              style={{ width: "100%", height: "100%" }}
-            />
-          </div>
-        ) : null}
-        <div className="barra d-flex flex-row ml-4 mt-3">
-          <div className="icons d-flex flex-row mb-4">
-            <i className="fa fa-heart" />
-            <p>
-              {" "}
-              {this.props.data !== undefined && this.props.data !== null
-                ? this.props.data.fav_count
-                : null}{" "}
-            </p>
-            <i className="fa fa-retweet" />
-            <p>
-              {" "}
-              {this.props.data !== undefined && this.props.data !== null
-                ? this.props.data.ret_count
-                : null}{" "}
-            </p>
-          </div>
-          <div className=" d-flex flex-row justify-content-end hashtags mr-4">
-            {this.props.data !== undefined && this.props.data !== null
-              ? this.props.data.tags
-                  .splice(0, Math.min(5, this.props.data.tags.length))
-                  .map(tag => (
-                    <p className="bg-secondary text-white  text-center align-middle mr-1 first m-2">
-                      #{tag}
-                    </p>
-                  ))
-              : null}
-            <p className="bg-blue text-white  text-center align-middles mr-1 second  m-2">
-              ...
-            </p>
+            <div className="barra d-flex flex-row ml-4 mt-3 justify-content-between align-items-baseline flex-wrap">
+              <div className="icons d-flex flex-row mb-4 ">
+                <i className="fa fa-heart mr-2" />
+                <p>
+                  {" "}
+                  {this.props.data !== undefined && this.props.data !== null
+                    ? this.props.data.fav_count
+                    : null}{" "}
+                </p>
+                <div className="fa fa-retweet mr-2 ml-4" />
+                <p>
+                  {" "}
+                  {this.props.data !== undefined && this.props.data !== null
+                    ? this.props.data.ret_count
+                    : null}{" "}
+                </p>
+              </div>
+              <div className=" d-flex flex-row ml-2 mb-4">
+                <div className="hashtags">
+                  {this.props.data !== undefined && this.props.data !== null
+                    ? this.props.data.tags
+                        .splice(0, Math.min(5, this.props.data.tags.length))
+                        .map(tag => (
+                          <p className="bg-secondary text-white  text-center align-middle mr-1 first m-2  ">
+                            #{tag}
+                          </p>
+                        ))
+                    : null}
+                  <p className="bg-blue text-white  text-center align-middles mr-1 second  m-2 ">
+                    ...
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
