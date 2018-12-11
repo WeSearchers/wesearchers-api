59e7d16e00da06c664b72b140325a43a9626b914
Unknown <teste@noreply.com>
2018-12-11 16:02:32 +0000

    fix design forgotPassword and resetPassword

--- project/frontend/src/components/Login/resetPassword.js
+++ project/frontend/src/components/Login/resetPassword.js
@@ -1,31 +1,31 @@
 import React, { Component } from "react";
-import Request from '../../request';
+import Request from "../../request";
 
 class ResetPassword extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      new_password1: '',
-      new_password2: '',
-    }
+      new_password1: "",
+      new_password2: ""
+    };
   }
 
   handleChange = event => {
-    this.setState({[event.target.name]: event.target.value});
+    this.setState({ [event.target.name]: event.target.value });
   };
 
   handleSave = event => {
     event.preventDefault();
     let guid = new URLSearchParams(window.location.search).get("guid");
-      let fd = new FormData();
-      for(let elem in this.state){
-        fd.append(elem, this.state[elem]);
-      }
-      fd.append("guid", guid);
-      Request.post("api/user/reset", fd).then( response => {
-        if (response.status === 200)
-          window.location.assign(window.location.origin + "/login");
-      })
+    let fd = new FormData();
+    for (let elem in this.state) {
+      fd.append(elem, this.state[elem]);
+    }
+    fd.append("guid", guid);
+    Request.post("api/user/reset", fd).then(response => {
+      if (response.status === 200)
+        window.location.assign(window.location.origin + "/login");
+    });
   };
 
   render() {
@@ -35,6 +35,9 @@ class ResetPassword extends Component {
         <div className="container">
           <div className="login-form change-password">
             <div className="title">Create a new Password</div>
+            <div className="sub-title">
+              Enter a new password for your account
+            </div>
             {/* jmmonteiro criar nova password (ligação ao backend) */}
             <form onSubmit={this.handleSave}>
               <div className="input-password">
@@ -55,7 +58,7 @@ class ResetPassword extends Component {
                   required
                 />
               </div>
-              <input type="submit" value="save"/>
+              <input type="submit" value="save" />
             </form>
           </div>
         </div>
